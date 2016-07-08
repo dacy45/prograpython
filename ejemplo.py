56cb4458a074ed34a7690683e53cad66aa081166
BaalJohan <dragon_9708@hotmail.com>
2016-07-07 20:05:55 -0500

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -3,4 +3,6 @@ print("SUMA")
 n1=int(input("Ingrese un numero entero: "))
 n2=int(input("Ingrese un segundo numero entero: "))
 s=n1+n2
+r=n1-n2
 print("La suma de ",n1," y ",n2,"es: ",s)
+print("La diferencia de ",n1," y ",n2," es: ",r)
